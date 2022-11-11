6e4796057e7fd3eda3a0474773662972336fa607
BrianGaray <brianchess18@gmail.com>
2022-11-11 07:39:40 -0300

    seo imagenes

---
+++ js/scroll.js
@@ -0,0 +1,38 @@
+const body = document.querySelector("body"),
+  sidebar = body.querySelector("nav"),
+  toggle = body.querySelector(".toggle"),
+  searchBtn = body.querySelector(".search-box"),
+  modeSwitch = body.querySelector(".toggle-switch"),
+  modeText = body.querySelector(".mode-text");
+
+toggle.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+});
+
+searchBtn.addEventListener("click", () => {
+  sidebar.classList.remove("close");
+});
+
+modeSwitch.addEventListener("click", () => {
+  body.classList.toggle("dark");
+
+  if (body.classList.contains("dark")) {
+    modeText.innerText = "Light mode";
+  } else {
+    modeText.innerText = "Dark mode";
+  }
+});
+
+//ScrollReveal
+
+sr = ScrollReveal({
+  reset: true,
+  distance: "100px",
+  delay: 500,
+});
+
+sr.reveal(".products", {
+  delay: 500,
+  distance: "20px",
+  duration:1500,
+});
