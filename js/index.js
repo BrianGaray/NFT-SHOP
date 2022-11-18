cbb82ef8186475c786701530e94ea3894424cf0d
BrianGaray <brianchess18@gmail.com>
2022-11-18 15:54:53 -0300

    test

--- js/index.js
+++ js/index.js
@@ -34,9 +34,9 @@ modeSwitch.addEventListener("click", () => {
 
 if (localStorage.getItem("dark-mode") === "true") {
   document.body.classList.add("dark");
-  btnSwitch.classList.add("active");
+ 
 } else {
   document.body.classList.remove("dark");
-  btnSwitch.classList.remove("active");
+
 }
 
--- js/scroll.js
+++ js/scroll.js
@@ -8,30 +8,44 @@ window.sr = ScrollReveal({
 });
 
 sr.reveal("header", {
+  delay: 200,
+  duration: 1000,
   distance: "1px",
 });
 
 sr.reveal(".about", {
+  delay: 200,
+  duration: 1000,
   distance: "20px",
 });
 
 sr.reveal(".items", {
+  delay: 200,
+  duration: 1000,
   distance: "20px",
 });
 
 sr.reveal(".notable", {
+  delay: 200,
+  duration: 1000,
   distance: "20px",
 });
 
 sr.reveal(".swiper", {
+  delay: 200,
+  duration: 1000,
   distance: "20px",
 });
 
 sr.reveal(".collections", {
+  delay: 200,
+  duration: 1000,
   distance: "20px",
 });
 
 sr.reveal(".products", {
+  delay: 200,
+  duration: 1000,
   distance: "20px",
 });
 
