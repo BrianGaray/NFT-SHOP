57c77fb9748f116c884bf8431903ee17115b9931
BrianGaray <brianchess18@gmail.com>
2022-11-18 09:18:31 -0300

    sidebar update

--- js/index.js
+++ js/index.js
@@ -9,9 +9,7 @@ toggle.addEventListener("click", () => {
   sidebar.classList.toggle("close");
 });
 
-searchBtn.addEventListener("click", () => {
-  sidebar.classList.remove("close");
-});
+
 
 modeSwitch.addEventListener("click", () => {
   body.classList.toggle("dark");
@@ -31,22 +29,28 @@ const swiper = new Swiper(".swiper", {
   slidesPerView: 1,
   paginationClickable: true,
   spaceBetween: 10,
-
+  draggable: true,
+  
   autoplay: {
     delay: 3000,
   },
 
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
   },
-  draggable: true,
+  
   breakpoints: {
     1536: {
       slidesPerView: 4,
     },
-
+    1200: {
+      slidesPerView: 4,
+    },
     1024: {
+      slidesPerView: 3,
+    },
+    768: {
       slidesPerView: 3,
     },
     640: {
